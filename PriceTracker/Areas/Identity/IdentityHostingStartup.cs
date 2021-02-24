566f878ef7a4abf922b269d59044cf1b86aa01d5
Zunchy <zwaterbury1289@gmail.com>
2021-02-24 17:26:36 -0500

    Adding Identity Files to be Editable

---
+++ PriceTracker/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PriceTracker.Data;
+
+[assembly: HostingStartup(typeof(PriceTracker.Areas.Identity.IdentityHostingStartup))]
+namespace PriceTracker.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
