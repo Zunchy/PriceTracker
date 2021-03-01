17548cf94eae3f5aa06ac2ae04110a5345f80e78
Zunchy <zwaterbury1289@gmail.com>
2021-03-01 12:19:30 -0500

    Initial Template as to how the Web Scraping would be handled with a Scraper class to gether the data from a site. DataFormatter class to parse the data retrieved. and Item class to store Item details. Current  example implementation for Mercari

---
+++ PriceTracker/Data/DataFormater.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceTracker.Data
+{
+    public class DataFormater
+    {
+
+        public List<DisplayItem> FormatMercari(string data)
+        {
+            List<DisplayItem> returnList = new List<DisplayItem>();
+
+            List<String> dataItems = data.Split("END ITEM").ToList();
+
+            foreach(String item in dataItems)
+            {
+                List<String> itemInfo = item.Split("\r\n").ToList();
+                if(itemInfo.Count > 1)
+                {
+                    DisplayItem newItem = new DisplayItem();
+
+                    newItem.ItemName = itemInfo.First();
+
+                    List<String> priceInfo = itemInfo.Find(x => x.Contains('$')).Split(' ').ToList();
+
+                    if (!priceInfo.First().Contains('$'))
+                    {
+                        priceInfo.Remove(priceInfo.First());
+                    }
+
+                    newItem.ItemPrice = Int32.Parse(priceInfo.First().Substring(1, priceInfo.First().Length - 1));
+
+                    newItem.ItemLink = itemInfo.Last();
+
+                    returnList.Add(newItem);
+                }
+
+            }
+
+            return returnList;
+        }
+
+    }
+}
---
+++ PriceTracker/Data/Scraper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace PriceTracker.Data
+{
+    public class Scraper
+    {
+
+        ChromeOptions options;
+        IWebDriver driver;
+
+        public Scraper()
+        {
+            options = new ChromeOptions();
+            options.AddArguments("--headless");
+
+            driver = new ChromeDriver(options);
+        }
+
+        public String ScrapeMercari()
+        {
+
+            //mercari, eBid, ecrater, rubylane
+
+
+            // Search For Products
+  
+            driver.Navigate().GoToUrl("https://www.mercari.com/search/?keyword=dragonball");
+            //IReadOnlyCollection<IWebElement> test = driver.FindElements(By.TagName("li"));
+            IReadOnlyCollection<IWebElement> searchSpace = driver.FindElements(By.XPath("//div[@data-testid=\"ItemContainer\"]"));
+
+            String result = "";
+            foreach(IWebElement item in searchSpace)
+            {
+                IWebElement itemContent = item.FindElement(By.XPath("./.."));
+                String link = itemContent.GetAttribute("href");
+
+                result += itemContent.Text;
+                result += "\r\n";
+                result += link;
+                result += "END ITEM";
+    
+            }
+
+            return result;
+
+            /*
+            // Get Product Information 
+            using (IWebDriver driver = new ChromeDriver(options))
+            {
+
+                driver.Navigate().GoToUrl("https://www.mercari.com/us/item/m22856012925/");
+                IWebElement test = driver.FindElement(By.XPath("//*[contains(@class, 'RightColumn')]"));
+
+                String result = "";
+
+                result = test.Text;
+
+                return result;
+            }
+            */
+
+
+        }
+
+    }
+}
