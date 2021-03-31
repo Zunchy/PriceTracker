a597f75ed3ef20bfa184337b116dfad03a41132b
Robarge, Tyler <trobarge@kent.edu>
2021-03-31 17:09:31 -0400

    Fixed small issue in GetUserByPrinciple

--- PriceTracker/Services/TrackingService.cs
+++ PriceTracker/Services/TrackingService.cs
@@ -168,7 +168,7 @@ namespace PriceTracker.Data
             return _product.GetProduct(productId);
         }
 
-        public async Task GetUserByPrincipal(ClaimsPrincipal principal)
+        public async Task<ApplicationUser> GetUserByPrincipal(ClaimsPrincipal principal)
         {
             return await _userManager.GetUserAsync(principal);
         }
