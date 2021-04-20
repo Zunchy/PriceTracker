caea607a65c084290cbe56aa95eb42abf0a6900e
Zunchy <zwaterbury1289@gmail.com>
2021-04-20 11:16:57 -0400

    Creating MyProducts Page with Untracking Functionality

---
+++ PriceTracker/Pages/MyProductCardComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using PriceTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceTracker.Pages
+{
+    public partial class MyProductCardComponent
+    {
+        [Parameter] public MyProductCardComponent Product { get; set; }
+
+        [Parameter] public DisplayItem prodItem { get; set; }
+
+    }
+}
